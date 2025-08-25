0f7052e5b3a06e397001899cf7bffc7075f9f7b6
BiswanathBD <biswanath.sarker.bd@gmail.com>
2025-08-26 00:11:37 +0600

    set handle toggle for card click

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 function getById(id) {
   return document.getElementById(id);
 }
-getById("show-balnce").addEventListener("click", function () {
+getById("show-balance").addEventListener("click", function () {
   getById("balance").classList.remove("hidden");
   getById("hide").classList.add("hidden");
 
@@ -13,3 +13,37 @@ getById("show-balnce").addEventListener("click", function () {
 getById("log-out").addEventListener("click", function () {
   window.location.href = "./index.html";
 });
+
+// common features
+const commonPin = "4848";
+
+const sections = document.getElementsByClassName("dynamic-section");
+
+
+function hiddenToggle(id) {
+  for (let section of sections) {
+    section.classList.add("hidden");
+  }
+  getById(id).classList.remove("hidden");
+}
+// option card section
+// add money card
+const addCard = getById("add-card");
+addCard.addEventListener("click", function () {
+  hiddenToggle("add-money-section");
+});
+
+// cash-out card
+const outCard = getById("out-card");
+outCard.addEventListener("click", function () {
+  hiddenToggle("cash-out-section");
+});
+
+// transfer card
+const transferCard = getById("transfer-card");
+transferCard.addEventListener("click", function () {
+  hiddenToggle("transfer-section");
+});
+
+
+// add money feature
